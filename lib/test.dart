4580536403d5c6c936ddc2c4e5e32ea8eae3c482
samy <samyhisham992@gmail.com>
2024-10-13 12:12:35 +0400

    add validate List

--- lib/test.dart
+++ lib/test.dart
@@ -25,3 +25,31 @@
 // itemBuilder: (ctr, index) => ListTile(
 // title: Text(locationList[index]),
 // )));
+
+import 'package:flutter/material.dart';
+
+class Test extends StatelessWidget {
+  Test({super.key});
+
+  List<String> list = ['s', 'a', "s", 'a', 'c'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('${check(list)}'),
+    );
+  }
+
+  List<dynamic> check(List<dynamic> list) {
+    // Create a new list to store the result
+    List<dynamic> result = [];
+
+    for (int i = 0; i < list.length; i++) {
+      // Check if the result list is empty or if the last element is different
+      if (result.isEmpty || result.last != list[i]) {
+        result.add(list[i]); // Add the current element to the result
+      }
+    }
+    return result; // Return the modified list
+  }
+}
